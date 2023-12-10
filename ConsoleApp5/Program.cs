ad4c27c60ed6a350d9a0a076b68c35e48a080618
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-12-10 11:17:25 +0500

    Task0

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s0 = 0;
+            double s1 = 0;
+            int x = 2;
+            for (int j = 1; j <= 3; j++)
+            {
+                for (int k = 1; k <= 3; k++)
+                {
+                    s0 = (3 + k) * x;
+                    s1 += s0;
+                }
+            }
+
+            Console.WriteLine(Math.Round(s1,3));
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Tyuiu.GutsuAV.Sprint4.Task0.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GutsuAV.Sprint4.Task0.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GutsuAV.Sprint4.Task0.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("952dfbcd-b74b-4cd0-8dea-a2c3fd660794")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
